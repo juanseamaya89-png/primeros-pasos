05f303859f394d729656f66364ae6482c0d542bb
iamVibe <juanseamaya89@gmail.com>
2025-11-15 15:35:27 -0600

    Fix shuffle function array element swapping

--- script.js
+++ script.js
@@ -70,7 +70,7 @@ function resetApp() {
 function shuffle(arr) {
   for (let i = arr.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
-    [arr[i], arr[j]] = [arr[j]], [arr[i]];
+    [arr[i], arr[j]] = [arr[j], arr[i]];
   }
   return arr;
 }
